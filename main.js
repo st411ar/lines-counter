95816bb0aa8b618aea66e3adabc22bd4b9f97fc5
Vitaly Kiselev <st411ar@ya.ru>
2022-02-16 02:01:15 +0300

    version 1.0.5: clean code

--- main.js
+++ main.js
@@ -1,64 +1,42 @@
-const startTime = new Date()
-console.log('program start')
-
-let dirsNumber = 0
-let filesNumber = 0
-let linesNumber = 0
-
 const fs = require('fs')
 const path = require('path')
-const process = require('process')
 
+let dirs = 0
+let files = 0
+let lines = 0
 readDir('res')
 
-function readDir(dirName) {
-	fs.opendir(dirName, 'utf-8', (err, dir) => {
+function readDir(name) {
+	fs.opendir(name, 'utf-8', (err, dir) => {
 		if (err) throw err
-		readDirEntry(dir)
+		readNextDirEntry(dir)
 	})
 }
 
-function readDirEntry(dir) {
-	dir.read((err, dirent) => {
+function readNextDirEntry(dir) {
+	dir.read((err, dirEntry) => {
 		if (err) throw err
-		if (dirent) {
-			if (dirent.isFile()) readFile(dir.path, dirent.name)
-			if (dirent.isDirectory()) readSubDir(dir.path, dirent.name)
-			readDirEntry(dir)
-		} else {
-			console.log('Directory ' + dir.path + ' has bean read')
+		if (dirEntry) {
+			if (dirEntry.isFile()) {
+				files++
+				readFile(path.join(__dirname, dir.path, dirEntry.name))
+			}
+			if (dirEntry.isDirectory()) {
+				dirs++
+				readDir(dir.path + '/' + dirEntry.name)
+			}
+			readNextDirEntry(dir)
 		}
 	})
 }
 
-function readSubDir(dirPath, name) {
-	dirsNumber++
-	readDir(dirPath + '/' + name)
-}
-
-function readFile(filePath, name) {
-	filesNumber++
-	readLines(path.join(__dirname, filePath, name))
-}
-
-function readLines(filePath) {
+function readFile(filePath) {
 	fs.readFile(filePath, (err, content) => {
 		if (err) throw err
-		let lines = content.toString().split(/\r\n|\r|\n/).length
-		linesNumber += lines
-		console.log('file "' + filePath + '" number of lines: ' + lines)
+		lines += content.toString().split(/\r\n|\r|\n/).length
 	})
 }
 
-process.on('exit', (code) => {
-	console.log('directories: ' + dirsNumber)
-	console.log('files: ' + filesNumber)
-	console.log('lines: ' + linesNumber)
-	let linesPerFile = Math.round(linesNumber / filesNumber)
-	console.log('average lines per file: ' + linesPerFile)
-
-	console.log('Process exit event with code: ', code)
-	console.log('program exit')
-	let seconds = (new Date() - startTime) / 1000
-	console.log('elapsed time ' + seconds + ' seconds')
+require('process').on('exit', (code) => {
+	console.log('directories ' + dirs + ', files ' + files + ', lines ' + lines)
 });
